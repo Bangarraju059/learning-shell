63af2bed9b64801ff289d66c373e482ca21aaeb7
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-06 16:49:59 +0530

    Modified 03-vars.sh file

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,5 +2,14 @@
 
 a=10
 echo a
+date=2022-08-05
+echo Today date is $date
 
-echo $date
+date1=&(date +%F)
+echo Today date is $date1
+
+b=$((2+3-5*6))
+echo Value of b=$b
+
+#Env variable
+echo Value of c = $c
