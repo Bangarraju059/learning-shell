e1c08e06a2e0a3fdca49aaf2d0a67598641dc3e5
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-08 01:50:24 +0530

    Modified 05-functions.sh file | Added Variables

--- 05-functions.sh
+++ 05-functions.sh
@@ -3,7 +3,11 @@
 ## Define a function
 Sample() {
   echo sample function
+  echo  a from main program = $a
+  b = 20
 }
 
 ## Access a function, simply function name
+a = 10
 Sample
+echo b from function = $b
