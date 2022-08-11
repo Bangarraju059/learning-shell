df1e2279429087467d22e658b966bfbc6f4168bd
Bangar Raju T <bangaraju.59@gmail.com>
2022-08-11 20:27:07 +0530

    Updated 07-inputs.sh file

--- 07-inputs.sh
+++ 07-inputs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#If the number of arguments passed are less
+
+if [ $# -lt 2 ] ; then
+  echo Input missing
+  exit 1
+  fi
+
 echo 1st argument is = $1
 echo 2nd argument is = $2
 echo All arguments = $*
